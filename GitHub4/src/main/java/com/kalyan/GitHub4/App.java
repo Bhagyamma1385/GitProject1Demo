52cafd6043e4d6258f8ee1abc7a2489df832e440
Bhagyamma1385 <t.bhagyamma20@gmail.com>
2023-01-15 11:27:12 +0530

    cc

---
+++ GitHub4/src/main/java/com/kalyan/GitHub4/App.java
@@ -0,0 +1,13 @@
+package com.kalyan.GitHub4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
